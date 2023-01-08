1460d6993675db02b0706b0247bd4a9dc2772423
MatthiasGitHub2021 <80299309+MatthiasGitHub2021@users.noreply.github.com>
2023-01-07 17:49:17 -0700

    Temperature converter

---
+++ Activity 2/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Activity_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fahrbox = new System.Windows.Forms.TextBox();
+            this.celBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fahrbox
+            // 
+            this.fahrbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fahrbox.Location = new System.Drawing.Point(540, 70);
+            this.fahrbox.Multiline = true;
+            this.fahrbox.Name = "fahrbox";
+            this.fahrbox.Size = new System.Drawing.Size(183, 44);
+            this.fahrbox.TabIndex = 0;
+            this.fahrbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // celBox
+            // 
+            this.celBox.BackColor = System.Drawing.SystemColors.Menu;
+            this.celBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.celBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.celBox.Location = new System.Drawing.Point(540, 218);
+            this.celBox.Multiline = true;
+            this.celBox.Name = "celBox";
+            this.celBox.Size = new System.Drawing.Size(183, 44);
+            this.celBox.TabIndex = 1;
+            this.celBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(119, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(404, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Fahrenheit temperature:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(197, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(326, 33);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Temperature in Celsius:";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(540, 131);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(183, 57);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.btnClear.Location = new System.Drawing.Point(540, 294);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(183, 57);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear Form";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.celBox);
+            this.Controls.Add(this.fahrbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fahrbox;
+        private System.Windows.Forms.TextBox celBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Activity 2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            //set fahr to farBox value as text
+            string fahrenheit = fahrbox.Text;
+
+            //set f as double from fahr string
+            double f = double.Parse(fahrenheit);
+
+            //set celsius = f with conversion calculations
+            double celsius = (f - 32) * 5 / 9;
+
+            //set celBox = celsius
+            celBox.Text = celsius.ToString();
+        }
+
+        //clears boxes
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            fahrbox.Clear();
+            celBox.Clear();
+        }
+    }
+}
