b2a8b134d35696558fdacf22d7af9cfb7999a578
MatthiasGitHub2021 <80299309+MatthiasGitHub2021@users.noreply.github.com>
2023-01-07 18:30:04 -0700

    Seconds converter

---
+++ Activity 4/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Activity_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbUserInput = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(145, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a number:";
+            // 
+            // tbUserInput
+            // 
+            this.tbUserInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUserInput.Location = new System.Drawing.Point(374, 144);
+            this.tbUserInput.Name = "tbUserInput";
+            this.tbUserInput.Size = new System.Drawing.Size(191, 40);
+            this.tbUserInput.TabIndex = 3;
+            this.tbUserInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(302, 213);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(166, 75);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.tbUserInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbUserInput;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ Activity 4/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            //set numbers textbox to variable
+            string userEntry = tbUserInput.Text;
+
+            //set userEntry as numerical
+            int.TryParse(userEntry, out int entry);
+
+            //seconds converter to minutes, hours, days
+            if (entry < 60)
+            {
+                MessageBox.Show(entry + " seconds is " + entry + " seconds.");
+            }
+            else if (entry >= 60 && entry <= 3599)
+            {
+                MessageBox.Show(entry + " seconds is " + entry / 60 + " minutes.");
+            }
+            else if (entry >= 3600 && entry <= 85399)
+            {
+                MessageBox.Show(entry + " seconds is " + entry / 3600 + " hours.");
+            }
+            else if (entry >= 86400)
+            {
+                MessageBox.Show(entry + " seconds is " + entry / 86400 + " days.");
+            }
+        }
+    }
+}
