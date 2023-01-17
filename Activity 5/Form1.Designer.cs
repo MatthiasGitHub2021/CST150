f5876a09194005911ac2b5747e6e83e7ef9b7016
MatthiasGitHub2021 <80299309+MatthiasGitHub2021@users.noreply.github.com>
2023-01-17 12:15:40 -0700

    Strings

---
+++ Activity 5/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Activity_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tBResponse = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.InitialDirectory = "C:\\Users\\matth\\WorkspaceCST-150 C Sharp\\Activity 5";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(412, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 57);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Select File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tBResponse
+            // 
+            this.tBResponse.Location = new System.Drawing.Point(79, 27);
+            this.tBResponse.Multiline = true;
+            this.tBResponse.Name = "tBResponse";
+            this.tBResponse.Size = new System.Drawing.Size(306, 311);
+            this.tBResponse.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tBResponse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tBResponse;
+    }
+}
+
---
+++ Activity 5/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Tracing;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace Activity_5
+{
+    public partial class Form1 : Form
+    {
+        //open dialog control
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //filter for text docs only
+            openFileDialog.Filter = ("Text Document (.txt) |*.txt");
+
+            //write to text file
+            using (StreamWriter writer = File.AppendText("Activity 5 results.txt"))
+            {
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+
+                    //array to store words from file
+                    var filelocation = System.IO.File.ReadAllLines(openFileDialog.FileName);
+
+                    //array for lines
+                    List<string> lines = new List<string>(filelocation);
+
+                    //Lowercase message
+                    tBResponse.AppendText("Lower-case:\r\n");
+
+                    for (int i = 0; i < lines.Count; i++)
+                    {
+                        //lowercase response
+                        tBResponse.AppendText(lines[i].ToString().ToLower() + "\r\n");
+                        //append lower case to file
+                        writer.WriteLine(lines[i].ToString());
+                    }
+
+                    //Sort array and grab top
+                    lines.Sort();
+                    tBResponse.AppendText("\r\nFirst Word Alphabetically:\r\n" + lines[0].ToString());
+
+                    //append first to file
+                    writer.WriteLine(lines[0].ToString());
+
+                    //Grab last
+                    tBResponse.AppendText("\r\nLast Word Alphabetically:\r\n" + lines.Last().ToString());
+
+                    //append last to file
+                    writer.WriteLine(lines.Last().ToString());
+
+                    //Sort descending, grab top
+                    string longest = lines.OrderByDescending(l => l.Length).First();
+                    tBResponse.AppendText("\r\nLongest Word Alphabetically:\r\n" + longest);
+
+                    //append longest to file
+                    writer.WriteLine(longest);
+
+                    //counter var for most vowels
+                    int vowelCount = 0;
+
+                    //string var for result
+                    string mostVowels = "";
+
+                    //loops lines array
+                    for (int i = 0; i < lines.Count; i++)
+                    {
+                        //count for vowels
+                        int vowels = 0;
+
+                        //loops letters as j, j = a vowel word with a count
+                        for (int j = 0; j < lines[i].Length; j++)
+                        {
+                            if (lines[i][j] == 'a' ||
+                                lines[i][j] == 'e' ||
+                                lines[i][j] == 'i' ||
+                                lines[i][j] == 'o' ||
+                                lines[i][j] == 'u')
+                            {
+                                //each time vowel is found, ++
+                                vowels++;
+                            }
+                        }
+                        if (vowels > vowelCount)
+                        {
+                            vowelCount = vowels;
+                            //i that has the most vowels, sets = to mostVowels
+                            mostVowels = lines[i];
+                        }
+                    }
+                    //Most vowels response
+                    tBResponse.AppendText("\r\nMost Vowels Word:\r\n" + mostVowels);
+
+                    //append to file
+                    writer.WriteLine(mostVowels);
+                }
+            }
+            
+        }
+
+    }
+}
