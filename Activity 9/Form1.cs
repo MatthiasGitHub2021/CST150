d4b3a2c20c25dc59523fe5bc60fdbea18cbb648b
MatthiasGitHub2021 <80299309+MatthiasGitHub2021@users.noreply.github.com>
2023-01-29 15:02:51 -0700

    Method returns

---
+++ Activity 9/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Write a method that takes two integers and displays their sum.
+	    public int Sum(int one, int two) { int result = 0; return result; }
+
+        //Write a method that takes five doubles and returns their average.
+        public double Average(double one, double two, double three, double four, double five) { double result = 0; return result; }
+
+        //Write a method that returns the sum of two randomly generated integers.
+        public int RandomSumNumbers(int one, int two) { int result = 0; return result; }
+
+        //Write a method that takes three integers and returns true if their sum is divisible by 3, false otherwise.
+        public bool ThreeIntsDivisableByThree(int a, int b, int c) { return true; }
+
+        //Write a method that takes two strings and displays the string that has fewer characters.
+        public string FewerCharacters(string one, string two) { string result = ""; return result; }
+
+        //Write a method that takes an array of doubles and returns the largest value in the array.
+        public double LargestValueInDoubleArray(double[] doublesArray) { double result = 0; return result; }
+
+        //Write a method that generates and returns an array of fifty integer values.
+        public int[] ArrayOfFiftyIntegers() { int[] array = new int[49]; return array; }
+
+        //Write a method that takes two bool variables and returns true if they have the same value, false otherwise.
+        public bool SameValueBools(bool one, bool two) { return true; }
+
+        //Write a method that takes an int and a double and returns their product.
+        public double ReturnIntAndDoubleResult(int one, double two) { double product=0; return product; }
+
+        //Write a method that takes a two-dimensional array of integers and returns the average of the entries.
+        public int AverageOfTwoDimensionalArray(int[,] array) { int average = 0; return average; }
+
+    }
+}
